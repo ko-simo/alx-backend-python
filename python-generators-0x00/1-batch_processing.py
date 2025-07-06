3329963fcb2e79d94c9cf88828cc2ba6430f1c7f
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-06 15:19:12 +0100

    Update 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,20 +1,23 @@
 from seed import connect_to_prodev
 
 def stream_users_in_batches(batch_size):
+    """Yields rows in batches from user_data table"""
     connection = connect_to_prodev()
     cursor = connection.cursor(dictionary=True)
     offset = 0
+
     while True:
-        cursor.execute(f"SELECT * FROM user_data LIMIT {batch_size} OFFSET {offset}")
+        cursor.execute("SELECT * FROM user_data LIMIT %s OFFSET %s", (batch_size, offset))
         rows = cursor.fetchall()
         if not rows:
             break
-        yield rows  
         offset += batch_size
+        yield rows  # ✅ هذا هو المطلوب
     cursor.close()
     connection.close()
 
 def batch_processing(batch_size):
+    """Processes each batch and prints users over age 25"""
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user['age'] > 25:
