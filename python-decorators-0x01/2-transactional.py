cbcaad11ea8ff1eea760b832ce21a238221f70a0
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-13 14:19:48 +0100

    Create 2-transactional.py

---
+++ python-decorators-0x01/2-transactional.py
@@ -0,0 +1,22 @@
+def transactional(func):
+    @functools.wraps(func)
+    def wrapper(conn, *args, **kwargs):
+        try:
+            result = func(conn, *args, **kwargs)
+            conn.commit()
+            return result
+        except Exception as e:
+            conn.rollback()
+            print(f"[ERROR] Transaction failed: {e}")
+            raise
+    return wrapper
+
+# استخدم ديكوراتور من Task 1 أيضًا
+@with_db_connection
+@transactional
+def update_user_email(conn, user_id, new_email):
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET email = ? WHERE id = ?", (new_email, user_id))
+
+# Test
+update_user_email(user_id=1, new_email='new_email@example.com')
