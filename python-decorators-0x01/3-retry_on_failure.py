636fcfddf8587d305ebb7e0b4b125e6161955241
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-13 14:20:48 +0100

    Create  3-retry_on_failure.py

---
+++ python-decorators-0x01/3-retry_on_failure.py
@@ -0,0 +1,27 @@
+import time
+
+def retry_on_failure(retries=3, delay=2):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            for attempt in range(1, retries + 1):
+                try:
+                    return func(*args, **kwargs)
+                except Exception as e:
+                    print(f"[WARN] Attempt {attempt} failed: {e}")
+                    if attempt == retries:
+                        raise
+                    time.sleep(delay)
+        return wrapper
+    return decorator
+
+@with_db_connection
+@retry_on_failure(retries=3, delay=1)
+def fetch_users_with_retry(conn):
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM users")
+    return cursor.fetchall()
+
+# Test
+users = fetch_users_with_retry()
+print(users)
