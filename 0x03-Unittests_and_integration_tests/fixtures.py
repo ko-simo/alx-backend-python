9e698e808506155729792a77bcca99c21bd6b010
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-19 17:46:03 +0100

    Create fixtures.py

---
+++ 0x03-Unittests_and_integration_tests/fixtures.py
@@ -0,0 +1,17 @@
+
+"""Fixtures used for integration testing."""
+
+org_payload = {
+    "login": "google",
+    "id": 1,
+    "repos_url": "https://api.github.com/orgs/google/repos"
+}
+
+repos_payload = [
+    {"id": 1, "name": "repo1", "license": {"key": "apache-2.0"}},
+    {"id": 2, "name": "repo2", "license": {"key": "mit"}},
+    {"id": 3, "name": "repo3"}  # No license
+]
+
+expected_repos = ["repo1", "repo2", "repo3"]
+apache2_repos = ["repo1"]
