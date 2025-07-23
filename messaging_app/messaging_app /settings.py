c9d037cadbe5374c957d187fb678aaacea33615f
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-23 18:13:34 +0100

    Update settings.py

--- messaging_app/messaging_app /settings.py
+++ messaging_app/messaging_app /settings.py
@@ -140,3 +140,21 @@ REST_FRAMEWORK = {
         'rest_framework.authentication.BasicAuthentication',
     ],
 }
+
+
+
+
+INSTALLED_APPS += [
+    'rest_framework',
+    'rest_framework_simplejwt',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
