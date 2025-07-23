f9b4ea1062bc0c535e681804cfc51ae46f8f02fd
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-23 18:14:42 +0100

    Create auth.py

---
+++ messaging_app/chats /auth.py
@@ -0,0 +1,7 @@
+# auth.py
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+
+class CustomJWTAuthentication(JWTAuthentication):
+    """Custom JWT authentication logic can go here if needed"""
+    pass
