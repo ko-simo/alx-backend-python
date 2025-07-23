70777e8d3a17c141568ccf43905baff5c2e3006a
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-23 18:14:09 +0100

    Update urls.py

--- messaging_app/messaging_app /urls.py
+++ messaging_app/messaging_app /urls.py
@@ -16,3 +16,21 @@ urlpatterns = [
     path('api/', include('chats.urls')),  # ✅ Required: "api/"
     path('api-auth/', include('rest_framework.urls')),  # DRF login view
 ]
+
+
+
+# urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('messaging_app.chats.urls')),  # assuming chats app has its urls
+]
