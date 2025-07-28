ad11733fe5143020da5d0d6094f0c530539d5650
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-28 14:42:01 +0100

    Update views.py

--- messaging_app/chats /views.py
+++ messaging_app/chats /views.py
@@ -66,8 +66,11 @@
 
 
 from rest_framework import viewsets
-from .models import Conversation, Message
-from .serializers import ConversationSerializer, MessageSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Message
+from .serializers import MessageSerializer
+from .pagination import MessagePagination
+from .filters import MessageFilter
 from .permissions import IsParticipantOfConversation
 from rest_framework.permissions import IsAuthenticated
 
@@ -79,10 +82,13 @@ class ConversationViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         return Conversation.objects.filter(participants=self.request.user)
 
+
 class MessageViewSet(viewsets.ModelViewSet):
-    queryset = Message.objects.all()
     serializer_class = MessageSerializer
     permission_classes = [IsAuthenticated, IsParticipantOfConversation]
+    pagination_class = MessagePagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = MessageFilter
 
     def get_queryset(self):
         return Message.objects.filter(conversation__participants=self.request.user)
