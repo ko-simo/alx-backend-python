ffcc90cea4b393f2c359fb873e92b36370b855c6
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-07-28 14:35:36 +0100

    Update permissions.py

--- messaging_app/chats /permissions.py
+++ messaging_app/chats /permissions.py
@@ -7,3 +7,23 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return obj.user == request.user
+
+
+
+# chats/permissions.py
+
+from rest_framework import permissions
+from .models import Conversation
+
+class IsParticipantOfConversation(permissions.BasePermission):
+    """
+    Allows access only to participants in a conversation.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        user = request.user
+        if hasattr(obj, 'participants'):
+            return user in obj.participants.all()
+        elif hasattr(obj, 'conversation'):
+            return user in obj.conversation.participants.all()
+        return False
