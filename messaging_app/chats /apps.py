08705e808c1a8644356e9df0a998f7027759ae0a
ELMANSORI Mohamed <Kosimo632@gmail.com>
2025-08-03 16:12:00 +0100

    Delete messaging_app directory

--- messaging_app/chats /apps.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Apps config for chats."""
-
-from django.apps import AppConfig
-
-
-class ChatsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'chats'
--- messaging_app/chats /auth.py
+++
@@ -1,7 +0,0 @@
-# auth.py
-
-from rest_framework_simplejwt.authentication import JWTAuthentication
-
-class CustomJWTAuthentication(JWTAuthentication):
-    """Custom JWT authentication logic can go here if needed"""
-    pass
